88e211d383a7caec6c0813f766766237ec64d89b
masaaki <67402705+Masaaki-75@users.noreply.github.com>
2025-09-13 17:07:05 +0800

    <REF> cleanup example paths

--- local_openclip/constants.py
+++ local_openclip/constants.py
@@ -1,6 +1,6 @@
 OPENAI_DATASET_MEAN = (0.48145466, 0.4578275, 0.40821073)
 OPENAI_DATASET_STD = (0.26862954, 0.26130258, 0.27577711)
-VITAMIN_CKPT = '/inspire/hdd/ws-c6f77a66-a5f5-45dc-a4ce-1e856fe7a7b4/project/public/clmanew/weights/ViTamin-B/pytorch_model.bin'
-BIOMEDCLIP_CKPT = '/inspire/hdd/ws-c6f77a66-a5f5-45dc-a4ce-1e856fe7a7b4/project/public/clmanew/weights/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224/open_clip_pytorch_model.bin'
-BIOMEDCLIP_CFG = '/inspire/hdd/ws-c6f77a66-a5f5-45dc-a4ce-1e856fe7a7b4/project/public/clmanew/weights/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224/open_clip_config.json'
-BIOMEDBERT_DIR = '/inspire/hdd/ws-c6f77a66-a5f5-45dc-a4ce-1e856fe7a7b4/project/public/clmanew/weights/BiomedNLP-BiomedBERT-base-uncased-abstract'
+VITAMIN_CKPT = '../weights/ViTamin-B/pytorch_model.bin'
+BIOMEDCLIP_CKPT = '../weights/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224/open_clip_pytorch_model.bin'
+BIOMEDCLIP_CFG = '../weights/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224/open_clip_config.json'
+BIOMEDBERT_DIR = '../weights/BiomedNLP-BiomedBERT-base-uncased-abstract'
